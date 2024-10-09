dea0f7ea307bbd031a8ce665467025097e994ac6
ella-an2 <ella.an@mths.ca>
2024-10-09 11:15:09 -0400

    programmed the program

--- main.ts
+++ main.ts
@@ -1,8 +1,49 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ella
+ * Created on: Sep 2024
+ * This program plays rock paper scissors 
 */
 
-basic.showString('Hello, World!')
+let randomNumber: number = 0
+let currentScore: number
+
+randomNumber = -1
+currentScore = 0
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0,2)
+    basic.clearScreen()
+
+
+    if (randomNumber == 0) {
+    basic.showIcon(IconNames.SmallSquare)
+    }
+
+    if (randomNumber == 1) {
+    basic.showIcon(IconNames.Square)
+    }
+
+    if (randomNumber == 2) {
+    basic.showIcon(IconNames.Scissors)
+    }
+
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.A, function () {
+    currentScore = currentScore + 1
+    basic.showIcon(IconNames.Yes)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.pause(1000)
+    basic.showString("Score:" + (currentScore).toString())
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+})
